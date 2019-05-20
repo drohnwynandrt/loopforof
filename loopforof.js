fec0ddbd6c4ce3aacf84d7cf33f288cc8131c09d
drohnwynandrt <dw.ramdattewari@gmail.com>
2019-05-20 09:32:26 +0200

    Added an array and HTML selection menu

---
+++ loopforof.js
@@ -0,0 +1,14 @@
+var pokemonArray = 
+[
+    {name:"Charizard", type:"Fire"}
+    {name:"Blastoise", type:"Water"}
+    {name:"Venusaur", type:"Grass"}
+    {name:"Blaziken", type:"Fire"}
+    {name:"Gloom", type:"Grass"}
+    {name:"Meganium", type:"Grass"}
+    {name:"Gyarados", type:"Water"}
+    {name:"Houndoom", type:"Fire"}
+    {name:"Lapras", type:"Water"}
+    {name:"Greninja", type:"Water"}
+    {name:"Ninetales", type:"Water"}
+]
