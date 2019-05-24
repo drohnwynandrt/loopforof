1be7833bab5fba5c484b042f664c9629997ce822
drohnwynandrt <dw.ramdattewari@gmail.com>
2019-05-24 15:12:29 +0200

    Fixed the display in div

--- loopforof.js
+++ loopforof.js
@@ -1,20 +1,22 @@
- var pokemonArray = ["Charizard", "Ninetales", "Blastoise", "Venusaur"] 
-
- function pokemon(){
-    for (var i=0; i < pokemonArray.length; i++){
-     for (poke of pokemonArray){
-         console.log(pokemonArray[i])
-     }
- }}
-
-
-
-/*
+ var pokemonArray = [
+    {name:"Charizard", type:"Fire"},
+    {name:"Blastoise", type:"Water"},
+    {name:"Venusaur", type:"Grass"},
+    {name:"Blaziken", type:"Fire"},
+    {name:"Gloom", type:"Grass"},
+    {name:"Meganium", type:"Grass"},
+    {name:"Gyarados", type:"Water"},
+    {name:"Houndoom", type:"Fire"},
+    {name:"Lapras", type:"Water"},
+    {name:"Greninja", type:"Water"},
+    {name:"Ninetales", type: "Fire"}
+] 
+function chooseType(){
+document.getElementById("output").innerHTML ="Pokemon: <br />";
 var showType = document.getElementById("chooseType").value;
 
-for (var i=0; i < pokemonArray.length; i++)
+for (var i=0; i < pokemonArray.length; i++){
 if (showType == pokemonArray[i].type){
-    document.getElementById("output").innerHTML += pokemonArray[i].name +"<br />";
+ document.getElementById("output").innerHTML += pokemonArray[i].name +"<br />";
+}}
 }
-}
-*/
